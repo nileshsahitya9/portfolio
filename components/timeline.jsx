c4b26d6194ace4297e429d4b29d147a7b7f573a3
nileshsahitya9 <36760635+nileshsahitya9@users.noreply.github.com>
2020-03-22 19:43:51 +0530

    Add files via upload

---
+++ components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>SIH hackathon Participant <span>2019-present</span></h2>
+                        <p>My team Datacops has been selected in a 2019 grand finale of smart India hackathon , our project is on women safety</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation at Techno India Njr <span>2017-2021</span></h2>
+                        <p>I am pursuing my under-graduation studies from Techno India NJR Institute of Technology major part of studies that is web development, DS algorithm, mobile app development, some basic concepts of oops,c,c++,python </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2005-2017</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 77 percentage. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
